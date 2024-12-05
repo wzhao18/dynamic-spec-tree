2ec73c353b9891ad51b559218a289a222f445d4a
wzhao18 <wzhao18.sz@gmail.com>
2024-12-05 08:03:38 +0000

    fix

--- tests/testbed.py
+++ tests/testbed.py
@@ -129,7 +129,7 @@ def simulation_baseline(target_model : GraphInferenceEngineTG, dataloader: DataL
             start_length = 0
             while inner_decoding_step < 32 and terminate == False:
 
-                print(input_ids.shape)
+                # print(input_ids.shape)
 
                 if inner_decoding_step == 0:
                     start_length = input_ids.shape[1]
@@ -278,7 +278,7 @@ else:
     # tree_size = args.tree_size
     tree_size = grow_map["size"]
 
-    print(tree_size)
+    # print(tree_size)
     # [[0], [1, 2, 3, 4, 5, 6, 7, 8], [9, 10, 11, 12, 13, 14, 15, 16, 17, 18], [19, 20, 21, 22, 23, 24, 25], [26, 27, 28, 29, 30], [31]]
     idx_lists = grow_map["roots"]
 
